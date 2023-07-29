4a81a29c61be5b6f4fe18750495c4c0fece72a49
MERVE ŞENYİĞİT <merve.senyigit@outlook.com>
2023-07-30 02:06:59 +0300

    8. dersin sonu

---
+++ EasyCashIdentityProject.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        //DATAACCESSDEKİ IGENERİCDAL dakilerin aynısı ayırmak için t koydum.
+        void TInsert(T t);
+        void TDelete(T t);
+
+        void TUpdate(T t);
+
+        T TGetById(int id);
+        List<T> TGetList();
+    }
+}
--- EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -12,10 +12,19 @@ namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidatio
     public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
         //bu raya AppUSER yazmammız sağlıklı olmaz kullanmayacağımız properyler var.
     {
-        //ben burada Rulefor'a erişim sağlayamıyorum. yapıcı metottla geliyor
+        //ben burada Rulefor'a erişim sağlayamıyorum. yapıcı metotla geliyor
         public AppUserRegisterValidator()
         {
-            
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez.");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez.");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez.");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girişi yapın");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir email adresi giriniz");
         }
     }
 }
